ff3cbf99b3e6365c0dace9a9662331713a200593
xyz888 <811593937@qq.com>
2020-05-06 02:52:20 +0800

    cdn优化

--- js/data.js
+++ js/data.js
@@ -1,22 +1,18 @@
 
     	
 		new Vue({
-		  el: '#lunbo',
+		  el: '#app',
 		  data: {
-            sites: [
-			  { url: 'img/2.jpg' ,href : '' },
-		      { url: 'img/1.jpg' ,href : 'https://im.vsolution.online/landing/share.html?promote=19321797' },  //爱钱
+			cdn: true,
+			cdn_url: 'https://cdn.jsdelivr.net/gh/xyz888/appsw@v1.0/',
+            lunbo: [
+				{ url: 'img/2.jpg' ,href : '' },
+				{ url: 'img/1.jpg' ,href : 'https://im.vsolution.online/landing/share.html?promote=19321797' },  //爱钱
 		     
-		      // { url: 'img/3.jpg' ,href : 'http://www.baidu.com' }
-			  // { url: 'img/4.jpg' ,href : 'https://chanshike.cn/?uid=127825' },  //禅试客
-		    ]
-        	}
-		})
-
-		new Vue({
-		  el: '#pingtai',
-		  data: {
-		    sites: [
+		        // { url: 'img/3.jpg' ,href : 'http://www.baidu.com' }
+			    // { url: 'img/4.jpg' ,href : 'https://chanshike.cn/?uid=127825' },  //禅试客
+		    ],
+			pingtai: [
 				{ ico: 'img/pp.jpg' ,name: '试玩平台1' ,desc: '多多红包' ,plus: '提现秒到' ,href : 'http://sdlw.pphulian.com/?r=818682617&d=20200504&en=hongshuitu&t=link&m=1&bt=1' },
 				{ ico: 'img/shike.png' ,name: '试玩平台2' ,desc: '试客' ,plus: '提现秒到' ,href : 'https://shike.com/i/QXomIfA' },
 				{ ico: 'img/qianmai.jpg' ,name: '试玩平台3' ,desc: '钱脉' ,plus: '提现秒到' ,href : 'https://m.qianmaiap.com/?u=1974360' },
@@ -26,17 +22,15 @@
 				{ ico: 'img/huojian.jpg' ,name: '试玩平台7' ,desc: '火箭' ,plus: '任务最多' ,href : 'https://huojianshiwan.com/?from=singlemessage&isappinstalled=0#/link/SDd6eUZQcFY1cTdIS1hRYzYyU2ZWUT09'},
 				{ ico: 'img/diaoqianyan.png' ,name: '试玩平台8' ,desc: '掉钱眼儿' ,plus: '新任务多' ,href : 'http://invite.chuangqish.cn/?userId=10351479&e=462&source=6'},
 				
-				
 		      
-		      
-		      
-		      
-		      /*{ ico: 'img/qianzhuang.jpg' ,name: '钱庄' ,href : 'http://q.zgkeyou.com/download?fromuid=1793432' },
-		      { ico: 'img/neihanhongbao.png' ,name: ' 内涵红包' ,href : 'https://app.xiaohanapp.com/Home/SharPage/1366044' },
-		      { ico: 'img/lanmao.png' ,name: '懒猫' ,href : 'http://cat.cattryapp.com/u/2326804.html' },
-		      { ico: 'img/jubaopeng.png' ,name: '巨宝朋' ,href : 'http://im.jubaopeng9999.com/jbph5/api/sharePage/17119306' },
-		      { ico: 'img/jinrizhuan.png' ,name: '今日赚' ,href : 'https://www.shangbangaso.cn/new-activity/index.html?uid=wrwto4_sign=1lzAgn9h/0Zbiz4tQZWpLbfhwNUipxNuAW8e6IuC4DU=_source=webViewShareNoPc_bundleId=com.posteroliver.NewZipCode' },
-			  */
+				/*{ ico: 'img/qianzhuang.jpg' ,name: '钱庄' ,href : 'http://q.zgkeyou.com/download?fromuid=1793432' },
+				{ ico: 'img/neihanhongbao.png' ,name: ' 内涵红包' ,href : 'https://app.xiaohanapp.com/Home/SharPage/1366044' },
+				{ ico: 'img/lanmao.png' ,name: '懒猫' ,href : 'http://cat.cattryapp.com/u/2326804.html' },
+				{ ico: 'img/jubaopeng.png' ,name: '巨宝朋' ,href : 'http://im.jubaopeng9999.com/jbph5/api/sharePage/17119306' },
+				{ ico: 'img/jinrizhuan.png' ,name: '今日赚' ,href : 'https://www.shangbangaso.cn/new-activity/index.html?uid=wrwto4_sign=1lzAgn9h/0Zbiz4tQZWpLbfhwNUipxNuAW8e6IuC4DU=_source=webViewShareNoPc_bundleId=com.posteroliver.NewZipCode' },
+				*/
 		    ]
-		  }
+        	}
 		})
+
+
