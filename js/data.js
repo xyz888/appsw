2b51bfec686bbdb9e11ea4d84b7845c4dbfbd8b3
xyz888 <811593937@qq.com>
2020-05-15 00:24:24 +0800

    modify

--- js/data.js
+++ js/data.js
@@ -20,12 +20,17 @@
 			pingtai: [
 				{ ico: 'img/pp.jpg' ,name: '多多红包' ,desc: '提现秒到' ,href : 'http://sdlw.pphulian.com/?r=818682617&d=20200504&en=hongshuitu&t=link&m=1&bt=1' },
 				{ ico: 'img/shike.png' ,name: '试客' ,desc: '提现秒到' ,href : 'https://shike.com/i/QXomIfA' },
+				{ ico: 'img/qianka.jpg' ,name: '钱咖' ,desc: '单价最高' ,href : 'https://dawangde.me/?u=69546107&referer_code=bc87e230cd&v=20200515' },
 				{ ico: 'img/qianmai.jpg' ,name: '钱脉' ,desc: '极速提现' ,href : 'https://m.qianmaiap.com/?u=1974360' },
 				{ ico: 'img/chanshike.jpg' ,name: '禅试客' ,desc: '任务最多' ,href : 'http://csk.limayao.com/?uid=5227244' },
 				{ ico: 'img/xiaoyu.jpg' ,name: '小鱼' ,desc: '新手必做' ,href : 'http://wx.xy599.com/share.php?id=22821733&act_source=invite' },
 				{ ico: 'img/imoney.png' ,name: '爱钱' ,desc: '海量任务' ,href : 'https://im.vsolution.online/landing/share.html?promote=19321797' },
 				{ ico: 'img/huojian.jpg' ,name: '火箭' ,desc: '任务最多' ,href : 'https://huojianshiwan.com/?from=singlemessage&isappinstalled=0#/link/SDd6eUZQcFY1cTdIS1hRYzYyU2ZWUT09'},
 				{ ico: 'img/diaoqianyan.png' ,name: '掉钱眼儿' ,desc: '新任务多' ,href : 'http://invite.chuangqish.cn/?userId=10351479&e=462&source=6'},
+				{ ico: 'img/miaozhuan.jpg' ,name: '秒赚' ,desc: '大量任务' ,href : 'https://wx.ixincun.cn/u/97202376?type=4'},
+				{ ico: 'img/jinrizhuan.png' ,name: '今日赚' ,href : 'https://www.shangbangaso.cn/new-activity/index.html?uid=8a2rbo_sign=ttu/SJk6s5QD1RCANpav8Vv8DKaTSnS5H8XpFnKPjBc=_source=webViewShareNoPc_bundleId=com.convertplus.converter'},
+				
+
 				
 		      
 				/*{ ico: 'img/qianzhuang.jpg' ,name: '钱庄' ,href : 'http://q.zgkeyou.com/download?fromuid=1793432' },
